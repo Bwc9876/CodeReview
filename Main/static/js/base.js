b89c4504f4f9007594d60980c9cc3c16838b5d9b
bwc9876 <bwc9876@gmail.com>
2021-11-01 14:08:38 -0400

    Added docstrings for Main js files

--- Main/static/js/base.js
+++ Main/static/js/base.js
@@ -1,3 +1,8 @@
 $(document).ready(function (){
+    /**
+     * This function will run when the document loads.
+     * It sets the title of the page to whatever is in the #pageHeader element.
+     */
+
     document.title = `${$("#pageHeader").text()} | CodeReview`;
 });
