841ea1262f670f299b856db6a60d6110dae5f71f
Ben <bwc9876@gmail.com>
2021-11-09 09:37:47 -0500

    Added frontend form

---
+++ Instructor/static/js/user-list.js
@@ -0,0 +1,9 @@
+function cleanupSubmitClick() {
+    $("#cleanupSpinner").toggleClass("d-none", false);
+    $("#cleanupSubmitText").toggleClass("d-none", true);
+    //$("#cleanupForm").submit();
+}
+
+$(document).ready(function (){
+    $("#cleanupSubmitButton").click(cleanupSubmitClick);
+});
