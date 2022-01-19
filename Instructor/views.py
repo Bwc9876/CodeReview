d353341bf132259c2ff59c1051f74d2420d611bd
Ben C <bwc9876@gmail.com>
2022-01-19 09:43:34 -0500

    Changed The Way Reviewers Work

--- Instructor/views.py
+++ Instructor/views.py
@@ -5,6 +5,7 @@
 from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Q
 from django.http import Http404
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
@@ -108,11 +109,9 @@ class UserListView(LoginRequiredMixin, IsSuperUserMixin, TemplateView):
             This function defines back-end behaviour when the user uses the POST method.
             It updates sessions and reviewers based off user input.
         """
-
-        objs = list(self.get_queryset())
-        for index, user in enumerate(objs):
-            objs[index].is_reviewer = str(user.id) in self.request.POST.getlist('reviewers')
-        self.get_queryset().bulk_update(objs, ['is_reviewer'], batch_size=10)
+        reviewer_query = Q(id__in=self.request.POST.getlist('reviewers'))
+        self.get_queryset().filter(reviewer_query).update(is_reviewer=True)
+        self.get_queryset().fitler(~reviewer_query).update(is_reviewer=False)
         self.get_queryset().filter(id__in=self.request.POST.getlist("to_delete")).delete()
         messages.add_message(self.request, messages.SUCCESS, "Users Updated")
         return redirect("user-list")
