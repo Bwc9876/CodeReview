613f9a404d663f9f783536060ada6bc7e85f407f
Ben C <bwc9876@gmail.com>
2022-11-10 17:35:27 -0500

    Also add on edit page

--- Main/static/js/review_create.js
+++ Main/static/js/review_create.js
@@ -1,10 +1,9 @@
 $(document).ready(() => {
-
     const rubricDropdown = $("#id_rubric");
     const previewButton = $("#rubricPreviewButton");
-
-    rubricDropdown.change(() => previewButton.attr("disabled", rubricDropdown.val() === ""));
-
+    const checkForSelected = () => previewButton.attr("disabled", rubricDropdown.val() === "");
+    checkForSelected();
+    rubricDropdown.change(checkForSelected);
     previewButton.click(() => {
         $(".rubric-preview").toggleClass("d-none", true);
         $(`#rubric-${rubricDropdown.val()}`).toggleClass("d-none", false);
